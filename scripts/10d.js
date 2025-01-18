ce07d7bb3d3504e1e1ae9332095cdd7b98e06ddb
John Paul Varde <johnpaulvarde13@gmail.com>
2025-01-18 20:58:46 +0800

    Add a feature that 1 button can be ON at the time

--- scripts/10d.js
+++ scripts/10d.js
@@ -2,8 +2,20 @@ function buttons(selector) {
     const buttonElement = document.querySelector(selector);
 
     if (!buttonElement.classList.contains("is-toggled")) {
+        turnOfPreviousButton ()
       buttonElement.classList.add("is-toggled");
+     
     } else {
       buttonElement.classList.remove("is-toggled");
+    }
+
+  
+  }
+
+  function turnOfPreviousButton () {
+    const previousButton = document.querySelector('.is-toggled');
+
+    if (previousButton) {
+        previousButton.classList.remove('is-toggled')
     }
   }
